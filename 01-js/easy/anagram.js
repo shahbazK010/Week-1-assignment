5a6db3a99ca55159ab06c0d126926b5cec2b8183
Shahabaz khan <112203967+shahbazK010@users.noreply.github.com>
2023-06-13 12:37:10 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,17 @@
 */
 
 function isAnagram(str1, str2) {
+	str1 = str1.replace(/\s/g, "").toLowercase();
+	str2 = str2.replace(/\s/g, "").toLowercase();
+	
+	if(str1.length !== str2.length){
+		return false
+	}
 
+	const sortedStr1 = str1.split('').sort().join('')
+	const sortedStr2 = str2.split('').sort().join('')
+
+	return sortedStr1 ===sortedStr2
 }
 
 module.exports = isAnagram;
